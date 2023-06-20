8a611c9f0ad33b94c28a82c7d107779f8609212e
ARAJO <dddd2511u@gmail.com>
2023-06-20 10:36:20 +0900

    first commit

---
+++ src/main/java/com/example/study_servlets/HelloWorldServlet.java
@@ -0,0 +1,19 @@
+package com.example.study_servlets;
+
+
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/helloWorldServlet")
+public class HelloWorldServlet extends HttpServlet { 
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       System.out.println("HelloWorldServlet -doGet()");
+    }
+    
+}
