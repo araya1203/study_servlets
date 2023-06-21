a0b6d3b8c856e7db9c53c64545b33ff2530e9316
dancingkimDH <kimdonghyun0701@gmail.com>
2023-06-21 11:53:40 +0900

    updsate

--- src/main/java/com/example/study_servlets/controls/ConnectDBServlet.java
+++ src/main/java/com/example/study_servlets/controls/ConnectDBServlet.java
@@ -36,7 +36,7 @@ public class ConnectDBServlet extends HttpServlet {
             
             Commons commons = new Commons();
             String query1 = "INSERT INTO car_infors(CAR_NAME, YEAR, CAR_INFOR_ID, COMPANY_ID)\n" + //
-                    "VALUES ('name', '2000', '"+commons.generateUUID()+"', 'C001');";
+                    "VALUES ('VOLVO', '2000', '"+commons.generateUUID()+"', 'C002');";
 
             Statement statement1 = connection.createStatement();
             int resultset1 = statement1.executeUpdate(query1);
