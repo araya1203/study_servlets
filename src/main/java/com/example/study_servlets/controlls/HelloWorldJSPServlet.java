5bde3173919141aec853da6932c16261e61222bd
dancingkimDH <kimdonghyun0701@gmail.com>
2023-06-26 11:48:52 +0900

    update

--- src/main/java/com/example/study_servlets/controlls/HelloWorldJSPServlet.java
+++ src/main/java/com/example/study_servlets/controlls/HelloWorldJSPServlet.java
@@ -3,19 +3,27 @@ package com.example.study_servlets.controlls;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/helloWorldServlet")
+@WebServlet(urlPatterns = "/helloWorldJSPServlet")
 public class HelloWorldJSPServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         
         try {
             String contents = "Yoju Lab !";
             
+            request.setAttribute("contents", "contents");
+            // path변경해야 함
+            //setParam과 getParam 활용?
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("/helloworldJSP.jsp");
+            requestDispatcher.forward(request, response);
+
+
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
